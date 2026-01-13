4c5bab797fe492a4b9f5d2daf8abf02e2cbfeaba
ander.saarniit <ander.saarniit@ut.ee>
2026-01-13 15:29:13 +0200

    added OSM data to MapLibre and added camera element

--- GNSSFreeLocalization/src/screens/MapScreen.tsx
+++ GNSSFreeLocalization/src/screens/MapScreen.tsx
@@ -1,6 +1,26 @@
 import React from 'react';
-import { MapView } from "@maplibre/maplibre-react-native";
+import { 
+  MapView,
+  Camera,
+} from "@maplibre/maplibre-react-native";
 
 export default function MapScreen() {
-  return <MapView style={{ flex: 1 }} />;
+  return (
+    <MapView 
+      style={{ flex: 1 }}
+      mapStyle="https://tiles.openfreemap.org/styles/liberty" // OpenFreeMap Liberty style
+    >
+    <Camera
+      defaultSettings={{
+        centerCoordinate: [25.0, 58.6],
+        zoomLevel: 7,
+      }}
+      maxBounds={{
+        ne: [28.2, 59.8],
+        sw: [21.5, 57.4],
+      }}
+      minZoomLevel={7}
+    />
+    </MapView>
+  );
 }
